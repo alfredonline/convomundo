3a6c23be0bc2e645f346c17ade2626a51fe372c0
Alfie <87819682+alfredonline@users.noreply.github.com>
2026-02-01 13:43:38 +0000

    add production_api_url back in

--- app/src/constants/api.ts
+++ app/src/constants/api.ts
@@ -1,4 +1,4 @@
-const production_api_url = null;
+const production_api_url = "https://api.convomundo.com";
 const development_api_url = "http://localhost:3000"
 
 export { production_api_url, development_api_url };
